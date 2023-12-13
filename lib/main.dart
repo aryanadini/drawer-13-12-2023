89c342e37c75eb7a69ef50018e8abb8f1db6c51f
aryanadini <119577677+aryanadini@users.noreply.github.com>
2023-12-13 11:40:25 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:new4/myalert.dart';
+import 'package:new4/mydrawer.dart';
+import 'package:new4/navi.dart';
+
+import 'Tfalert.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Mydrawer(),
+    );
+  }
+}
+
+
